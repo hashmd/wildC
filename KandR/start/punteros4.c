9f352b89384d975bd3586772f53fe3de1100a0e0
Vanesa Garcia Romero <vanesa@ibeasoft.com>
2015-07-13 03:01:28 -0700

    copy

--- KandR/start/punteros4.c
+++ KandR/start/punteros4.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int strend(char *s, char *t);
-int strendPunteros(cahr *s, char *t);
+int strendPunteros(char *s, char *t);
 int largoChars(char *s);
+int largoCharsPuentero(char *s);
 
 int main(){
 	
 	char s[] = "hola mundo";
 	char t[] = "mundo";
 	char r[] = "jor";
-	printf("\n%s\" esta al final de  %s\": %d\n",r,t, strend(t, r));
-	printf("\n\"%s\" esta la final de \"%s\": %d\n", t,s, strend(s, t));
-	
+//	printf("\n\"%s\" esta al final de  \"%s\": %d\n",r,t, strendPunteros(t, r));
+//	printf("\n\"%s\" esta la final de \"%s\": %d\n", t,s, strend(s, t));
+	printf("\n%d", largoChars(s));
 }
 
 int largoChars(char *s){
@@ -26,7 +27,7 @@ int strend( char *s, char *t){
 	
 	int j = 0;
 	int i = largoChars(s)-largoChars(t);
-	
+	printf("%d\n i vale: ", i);
 	while(s[i] == t[j] && s[i] !='\0' && t[j] !='\0'){
 		j++;
 		i++;
@@ -40,20 +41,27 @@ int strend( char *s, char *t){
 
 int largoCharsPuntero(char *s){
 	int largo = 0;
-	while(*s !=0){
+	while(*s != '\0'){
 		largo++;
 		*s++;
 	}
 	return largo;
 }
 int strendPunteros(char *s, char *t){
-	*s = largoCharsPuntero(s)-largoCharPuntero(t);
-	while(*s != 0 && *t != 0) {
+	int n = largoChars(s)-largoChars(t);
+	printf("%d\n", n);
+	s += n;
+	while(*s != '\0' && *t != '\0') {
 		if(*s == *t){
 			*s++;
 			*t++;	
 		}
 		
 	}
-	return 0;	
+	if(*s=='\0' && *t=='\0'){
+		return 1;
+	}
+	else{
+		return 0;
+	}	
 }
